0fc4145fa2e24758c7b75f8dc3fda5f23cd08e66
nimmy <nimmyjohn05@gmail.com>
2020-04-03 15:26:13 +0530

    password encryption

--- LeaveManagementSystemRepository/LoginRepository.cs
+++ LeaveManagementSystemRepository/LoginRepository.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Data.SqlClient;
+using System.Security.Cryptography;
 using System.Text;
 
 namespace LeaveManagementSystemRepository
@@ -25,57 +26,68 @@ namespace LeaveManagementSystemRepository
 
         public Employee GetLogin(LoginDetails loginDetails)
         {
-            Employee employee = new Employee();
-
-            SqlConnection sqlconn = new SqlConnection(connectionString);
-            SqlCommand sqlcomm = new SqlCommand("loginvalidation");
-            sqlconn.Open();
-            sqlcomm.Connection = sqlconn;
-            sqlcomm.CommandType = CommandType.StoredProcedure;
-            sqlcomm.Parameters.AddWithValue("@username", loginDetails.Username);
-            sqlcomm.Parameters.AddWithValue("@password", loginDetails.Password);
-            SqlDataReader sdr = sqlcomm.ExecuteReader();
-            if (sdr.Read())
+            try
             {
-                employee.Id = (int)sdr["id"];
-                sdr.Close();
-                SqlCommand sqlcom = new SqlCommand("employeedetails");
-                sqlcom.Connection = sqlconn;
-                sqlcom.CommandType = CommandType.StoredProcedure;
-                sqlcom.Parameters.AddWithValue("@employeeid", employee.Id);
-                SqlDataReader datareader = sqlcom.ExecuteReader();
-                if (datareader.Read())
-                {
-                    employee.Id = (int)datareader["id"];
-                    employee.TypeId = (int)datareader["typeid"];
-                    employee.TypeName = datareader["etype"].ToString();
-                    employee.FirstName = datareader["firstname"].ToString();
-                    employee.MiddleName = datareader["middlename"].ToString();
-                    employee.LastName = datareader["lastname"].ToString();
-                    employee.Email = datareader["email"].ToString();
-                    employee.Salary = (int)datareader["salary "];
-                    employee.Username = datareader["username"].ToString();
-                    employee.LocationId = (int)datareader["locationid"];
-                    employee.LocationName = datareader["lname"].ToString();
-                }
-                //HttpContext.Session.SetString(SessionName, employee.Username)
+                var encryptedPassword = Encrypt(loginDetails.Password, "sblw-3hn8-sqoy19");
+                Employee employee = new Employee();
 
+                SqlConnection sqlconn = new SqlConnection(connectionString);
+                SqlCommand sqlcomm = new SqlCommand("LoginValidation");
+                sqlconn.Open();
+                sqlcomm.Connection = sqlconn;
+                sqlcomm.CommandType = CommandType.StoredProcedure;
+                sqlcomm.Parameters.AddWithValue("@Username", loginDetails.Username);
+                sqlcomm.Parameters.AddWithValue("@Password", encryptedPassword);
+                SqlDataReader sdr = sqlcomm.ExecuteReader();
+
+                if (sdr.Read())
+                {
+                    employee.Id = (int)sdr["id"];
+                    sdr.Close();
+                    SqlCommand sqlcom = new SqlCommand("EmployeeDetails");
+                    sqlcom.Connection = sqlconn;
+                    sqlcom.CommandType = CommandType.StoredProcedure;
+                    sqlcom.Parameters.AddWithValue("@employeeid", employee.Id);
+                    SqlDataReader datareader = sqlcom.ExecuteReader();
+                    if (datareader.Read())
+                    {
+                        employee.Id = (int)datareader["Id"];
+                        employee.TypeId = (int)datareader["Typeid"];
+                        employee.TypeName = datareader["Etype"].ToString();
+                        employee.FirstName = datareader["Firstname"].ToString();
+                        employee.MiddleName = datareader["Middlename"].ToString();
+                        employee.LastName = datareader["Lastname"].ToString();
+                        employee.Email = datareader["Email"].ToString();
+                        employee.Salary = (int)datareader["Salary "];
+                        employee.Username = datareader["Username"].ToString();
+                        employee.LocationId = (int)datareader["Locationid"];
+                        employee.LocationName = datareader["Lname"].ToString();
+                    }
+                    //HttpContext.Session.SetString(SessionName, employee.Username)
+                    
+                }
+                return employee;
             }
-            return employee;
+            catch (Exception e)
+            {
+                return null;
+            }
+         
         }
 
         public bool NewLogin(NewLogin newLogin)
         {
             //if (HttpContext.Session.GetString(SessionName) == null)
             //{
+                var encryptedPassword = Encrypt(newLogin.Password, "sblw-3hn8-sqoy19");
                 SqlConnection sqlconn = new SqlConnection(connectionString);
-                SqlCommand sqlComm = new SqlCommand("newLogin");
+                SqlCommand sqlComm = new SqlCommand("NewLogin");
                 sqlconn.Open();
                 sqlComm.Connection = sqlconn;
                 sqlComm.CommandType = CommandType.StoredProcedure;
                 sqlComm.Parameters.AddWithValue("@employeeId", newLogin.EmployeeId);
                 sqlComm.Parameters.AddWithValue("@username", newLogin.Username);
-                sqlComm.Parameters.AddWithValue("@password", newLogin.Password);
+                sqlComm.Parameters.AddWithValue("@password", encryptedPassword);
 
                 SqlParameter returnParameter = sqlComm.Parameters.Add("RetVal", SqlDbType.Int);
                 returnParameter.Direction = ParameterDirection.ReturnValue;
@@ -93,5 +105,18 @@ namespace LeaveManagementSystemRepository
            
             return returnValue;
         }
+        public static string Encrypt(string input, string key)
+        {
+            byte[] inputArray = UTF8Encoding.UTF8.GetBytes(input);
+            TripleDESCryptoServiceProvider tripleDES = new TripleDESCryptoServiceProvider();
+            tripleDES.Key = UTF8Encoding.UTF8.GetBytes(key);
+            tripleDES.Mode = CipherMode.ECB;
+            tripleDES.Padding = PaddingMode.PKCS7;
+            ICryptoTransform cTransform = tripleDES.CreateEncryptor();
+            byte[] resultArray = cTransform.TransformFinalBlock(inputArray, 0, inputArray.Length);
+            tripleDES.Clear();
+            return Convert.ToBase64String(resultArray, 0, resultArray.Length);
+        }
+       
     }
 }
